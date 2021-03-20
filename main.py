b170049cb806f9d80c716f3f32d6e044f1eb5d96
Christ Vice <47262681+ChristVice@users.noreply.github.com>
2021-03-20 03:46:12 -0500

    beta v1
    Changed icon
    Added quick sort
    Turns green after sorting completion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-import Sorting_Algorithms as sorts
 import time
 from tkinter import *
 
@@ -11,7 +10,8 @@ class Rectangle:
         self.width = width
         self.height = height
         self.x2 = x + width
-        self.color = "#00b200"  # "ffffff" "0f4a16" "00b200"
+        self.color = "#00b200"
+        self.outline = "#157145"
 
     def __str__(self):
         return f"h:{self.height}\t| x:{self.x} | y:{self.y} | x2:{self.x2}"
@@ -34,6 +34,7 @@ class SortingAlgorithm:
         self.window.config(bg="#ffffff")
         self.window.iconphoto(False, PhotoImage(file="images/icon.png"))
 
+        self.recs = []
         self.bars = []
         self.ranints = []
 
@@ -47,15 +48,26 @@ class SortingAlgorithm:
 
         self.window.mainloop()
 
-    def switch_bars(self, i1, i2):
+    def sorting_completed(self):
+        for i in range(len(self.bars)):
+            time.sleep(1 / (10 ** 10))
+            self.bar_canvas.create_rectangle(
+                self.bars[i].x,
+                self.bars[i].y,
+                self.bars[i].x2,
+                self.bars[i].height,
+                fill=self.bars[i].color,
+                outline=self.bars[i].color,
+                activefill="black",
+            )
+            self.bar_canvas.update()
 
+    def switch_bars(self, i1, i2):
         og_x = self.bars[i1].x
         des_x = self.bars[i2].x
 
-        while self.bars[i1].x != des_x:
-            rate = 3
-            time.sleep(1 / (10 ** 500))
-
+        rate = 1
+        while self.bars[i1].x != des_x or self.bars[i2].x != og_x:
             if self.bars[i1].x < des_x:  # moves bar to right
                 self.bar_canvas.move(self.recs[i1], rate, 0)  # moves i1 bar to i2
                 self.bars[i1].x += rate
@@ -65,7 +77,7 @@ class SortingAlgorithm:
                 self.bars[i2].x -= rate
                 self.bars[i2].x2 -= rate
 
-            else:
+            elif self.bars[i1].x > des_x:
                 self.bar_canvas.move(self.recs[i1], -rate, 0)
                 self.bars[i1].x -= rate
                 self.bars[i1].x2 -= rate
@@ -84,65 +96,54 @@ class SortingAlgorithm:
         )
 
     def Sorting_GUI(self):
+        x1 = 0
+        y1 = 2
+        gap = 2
+        rec_width = 1
 
-        self.recs = []
+        amount_of_bars = self.width // (rec_width + gap)
 
-        def bars_fill(amount=5, fill_all=False):
-            x1 = 0
-            y1 = 2
-            gap = 2
-            rec_width = 1
+        self.ranints = [x for x in range(1, amount_of_bars + 1)]
+        random.shuffle(self.ranints)
 
-            if fill_all:
-                amount_of_bars = self.width // (rec_width + gap)
-            else:
-                amount_of_bars = amount
+        for i in range(amount_of_bars):
+            self.bars.append(Rectangle(x1, y1, rec_width, self.ranints[i]))
+            x1 += rec_width + gap
 
-            self.ranints = [x for x in range(1, amount_of_bars + 1)]
-            random.shuffle(self.ranints)
-
-            for i in range(amount_of_bars):
-                self.bars.append(Rectangle(x1, y1, rec_width, self.ranints[i]))
-                x1 += rec_width + gap
-
-        def fill_canvas(amount_of_bars):
-            for i in range(amount_of_bars):
-                self.recs.append(
-                    self.bar_canvas.create_rectangle(
-                        self.bars[i].x,
-                        self.bars[i].y,
-                        self.bars[i].x2,
-                        self.bars[i].height,
-                        fill=self.bars[i].color,
-                        outline=self.bars[i].color,
-                        activefill="black",
-                    )
+        for i in range(amount_of_bars):
+            self.recs.append(
+                self.bar_canvas.create_rectangle(
+                    self.bars[i].x,
+                    self.bars[i].y,
+                    self.bars[i].x2,
+                    self.bars[i].height,
+                    outline=self.bars[i].outline,
+                    activefill="black",
                 )
+            )
 
-        bars_fill(100, True)
-        fill_canvas(len(self.bars))
-
-        # self.selection_sort()
-        self.quickSort(self.bars, 0, len(self.bars) - 1)
+        # self.selection_sort(self.bars)
+        self.quick_sort(self.bars, 0, len(self.bars) - 1)
+        self.sorting_completed()
 
     # _____________SORTING ALGORITHMS_____________
-    # SELECTION SORT
-    def selection_sort(self):  # works, sorts bars
-        for i in range(len(self.bars)):
+    # SELECTION SORT                        ---------------
+    def selection_sort(self, arr):  # works, sorts bars
+        for i in range(len(arr)):
             min_idx = i
-            for j in range(i + 1, len(self.bars)):
-                if self.bars[min_idx].height > self.bars[j].height:
+            for j in range(i + 1, len(arr)):
+                if arr[min_idx].height > arr[j].height:
                     min_idx = j
 
             self.switch_bars(min_idx, i)
 
-    # QUICK SORT
+    # QUICK SORT                            ---------------
     def partition(self, arr, low, high):
         i = low - 1
-        pivot = self.bars[high].height
+        pivot = arr[high].height
 
         for j in range(low, high):
-            if self.bars[j].height <= pivot:
+            if arr[j].height <= pivot:
 
                 i = i + 1
                 self.switch_bars(i, j)
@@ -150,14 +151,14 @@ class SortingAlgorithm:
         self.switch_bars(i + 1, high)
         return i + 1
 
-    def quickSort(self, arr, low, high):
-        if len(self.bars) == 1:
-            return self.bars
+    def quick_sort(self, arr, low, high):
+        if len(arr) == 1:
+            return arr
         if low < high:
-            pi = self.partition(self.bars, low, high)
+            pi = self.partition(arr, low, high)
 
-            self.quickSort(self.bars, low, pi - 1)
-            self.quickSort(self.bars, pi + 1, high)
+            self.quick_sort(arr, low, pi - 1)
+            self.quick_sort(arr, pi + 1, high)
 
 
 if __name__ == "__main__":
